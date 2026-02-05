e459e9a8617a2dc97dd9497f2c0c4978485d23bc
atulsingho <atulsingho457@gmail.com>
2026-02-05 12:46:11 +0600

    Insertion Sort Algorithm in Descending Order

---
+++ problem17.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,arr[50];
+    cout<<"Number of Elements: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]<key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+    cout<<"Insertion Sort: ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
