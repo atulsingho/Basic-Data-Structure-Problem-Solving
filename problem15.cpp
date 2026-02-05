0ff9fe34a5f69e2cfc4bd2223033960a66ea1a1f
atulsingho <atulsingho457@gmail.com>
2026-02-05 13:24:34 +0600

    Bubble Sort Algorithm in Descending Order
    Refactor bubble sort implementation for clarity.

--- problem15.cpp
+++ problem15.cpp
@@ -6,22 +6,25 @@ int main()
     int n,arr[50];
     cout<<"Number of Elements: ";
     cin>>n;
-    for(int i=0;i<n;i++)
+    // Taking input in array as the number of elements_______________________
+    for(int i=0;i<n;i++) 
     {
         cin>>arr[i];
     }
-    for(int i=0;i<n-1;i++)
+    //Bubble Sort_______________________________
+    for(int i=0;i<n-1;i++) // 'i' counts the number of passes. After each pass, one small element goes to its correct position which is at the end. At the last pass, the array is already sorted. So, i<n-1
     {
-        for(int j=0;j<n-i-1;j++)
+        for(int j=0;j<n-i-1;j++) // After each pass, the last elements are already sorted and we have to swap the unsorted elements. So, j<n-i-1
         {
-            if(arr[j]<arr[j+1])
+            if(arr[j]<arr[j+1]) // This condition is for descending order elements
             {
-                int temp = arr[j];
+                int temp = arr[j]; // Swapping the values using a temporary variable
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
             }
         }
     }
+    // Output____________________________
     cout<<"Bubble Sort: ";
     for(int i=0;i<n;i++)
     {
