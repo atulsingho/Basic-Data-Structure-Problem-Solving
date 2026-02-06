935c145b6d20bffd2b5f839ed46524cf22e99d41
atulsingho <atulsingho457@gmail.com>
2026-02-06 11:01:13 +0600

    Linear Search Algorithm

--- problem11.cpp
+++ problem11.cpp
@@ -8,20 +8,20 @@ int main()
     cin>>n;
     for(i=0;i<n;i++)
     {
-        cin>>arr[i];
+        cin>>arr[i]; //Array input
     }
     cout<<"Searching Value:";
     cin>>value;
     int count = 0;
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++) 
     {
-        if(arr[i]==value)
+        if(arr[i]==value) //Checks every element one by one and if it matches, then the value found at index 'i'
         {
             cout<<"Found at index "<<i;
-            count++;
+            count++; //It ensures that the value exists
         }
     }
-    if(count==0)
+    if(count==0) //if the value not found, it will enter this condition
     {
         cout<<"Not found.";
     }
