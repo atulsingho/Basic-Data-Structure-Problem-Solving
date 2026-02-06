5b83dbdbafb7244ea69b2f3948170682dbddaf7f
atulsingho <atulsingho457@gmail.com>
2026-02-06 11:07:28 +0600

    Add program to find the maximum in an array

--- problem04.c
+++ problem04.c
@@ -2,14 +2,14 @@
 int main()
 {
     int arr[] = {4,9,2,5,1};
-    int max = arr[0];
+    int max = arr[0]; //Consider as the first element is maximum
     for(int i=1;i<5;i++)
     {
-        if(max<arr[i])
+        if(max<arr[i]) //Compares with every element starting from the second
         {
-            max = arr[i];
+            max = arr[i]; //Makes the maximum if the intital value is greater than new value
         }
     }
-    printf("Maximum = %d",max);
+    printf("Maximum = %d",max); //prints the maximum value
     return 0;
 }
